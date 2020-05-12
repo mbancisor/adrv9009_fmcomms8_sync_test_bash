36c7246d87a02a0cca84d7bf4cf8c3daa5290c07
mbancisor <bancisor.mihai@gmail.com>
2020-05-12 22:18:57 +0000

    updates

--- jesd_status.sh
+++ jesd_status.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 file="log_jesd.txt"
-sample=$(($1*3))
+test=$(($1/3))
 
 echo >>$file
 echo >>$file
-echo "test_nr: $1 sample_nr: $sample" >>$file
+echo "test_nr: $test sample_nr: $1">>$file
 
-cat /sys/devices/platform/fpga-axi\@0/84a50000.axi-jesd204-rx/status
-cat /sys/devices/platform/fpga-axi\@0/84a50000.axi-jesd204-rx/lane0_info
-cat /sys/devices/platform/fpga-axi\@0/84a50000.axi-jesd204-rx/lane1_info
-cat /sys/devices/platform/fpga-axi\@0/84a50000.axi-jesd204-rx/lane2_info
-cat /sys/devices/platform/fpga-axi\@0/84a50000.axi-jesd204-rx/lane3_info
-cat /sys/devices/platform/fpga-axi\@0/84a50000.axi-jesd204-rx/lane4_info
-cat /sys/devices/platform/fpga-axi\@0/84a50000.axi-jesd204-rx/lane5_info
-cat /sys/devices/platform/fpga-axi\@0/84a50000.axi-jesd204-rx/lane6_info
-cat /sys/devices/platform/fpga-axi\@0/84a50000.axi-jesd204-rx/lane7_info
+cat /sys/devices/platform/fpga-axi\@0/84a50000.axi-jesd204-rx/status>>$file
+cat /sys/devices/platform/fpga-axi\@0/84a50000.axi-jesd204-rx/lane0_info>>$file
+cat /sys/devices/platform/fpga-axi\@0/84a50000.axi-jesd204-rx/lane1_info>>$file
+cat /sys/devices/platform/fpga-axi\@0/84a50000.axi-jesd204-rx/lane2_info>>$file
+cat /sys/devices/platform/fpga-axi\@0/84a50000.axi-jesd204-rx/lane3_info>>$file
+cat /sys/devices/platform/fpga-axi\@0/84a50000.axi-jesd204-rx/lane4_info>>$file
+cat /sys/devices/platform/fpga-axi\@0/84a50000.axi-jesd204-rx/lane5_info>>$file
+cat /sys/devices/platform/fpga-axi\@0/84a50000.axi-jesd204-rx/lane6_info>>$file
+cat /sys/devices/platform/fpga-axi\@0/84a50000.axi-jesd204-rx/lane7_info>>$file
